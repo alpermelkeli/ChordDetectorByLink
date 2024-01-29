9dcd7b52ff8de97dbf687c5f127f2720bd492c73
alpermelkeli <alpermelkeli@gmail.com>
2024-01-29 14:25:56 +0300

    data text format completed in use.py!

--- use.py
+++ use.py
@@ -1,10 +1,12 @@
+import sys
+
 import requests
 
 # Set the URL of your Flask app
-url = "http://192.168.1.39:5000/recognize"
+url = "http://3.81.66.38:5000/recognize"
 
 # Specify the YouTube video link you want to recognize
-youtube_link = "https://www.youtube.com/watch?v=r6xw6jlFlJg"
+youtube_link = sys.argv[1]
 
 # Create a dictionary to hold the form data
 data = {"link": youtube_link}
@@ -12,5 +14,18 @@ data = {"link": youtube_link}
 # Make a POST request to the /recognize route
 response = requests.post(url, data=data)
 
-# Print the response content
-print(response.text)
+# Function to format and write lab_content to a file
+def format_and_write_lab_content(data):
+    # Extract lab_content from the JSON response
+    lab_content = data.get("lab_content", "")
+
+    # Veriyi düzenle
+    formatted_data = lab_content.replace("\t", ",")  # Sekme karakterini virgül ile değiştir
+
+    # Dosyaya yaz
+    with open("formatted_data.txt", "w") as file:
+        file.write(formatted_data)
+    print("Veri başarıyla düzenlendi ve formatted_data.txt dosyasına yazıldı.")
+
+# Call the function with the response content
+format_and_write_lab_content(response.json())
